c4d339e5a9e8d746b6404cbfa34df032bf217b01
Sanpi <sanpi@homecomputing.fr>
2013-07-16 01:38:28 +0200

    Fix sources

--- src/main.v
+++ src/main.v
@@ -1,19 +1,23 @@
-module blink(clk, led);
+module blink(input wire clk, output reg led);
 
-input clk;
-output led;
+reg [31:0] cnt;
 
-reg led = 0;
-reg [25:0] counter = 0;
-
-always @(posedge clk) begin
-    if (counter == 0) begin
-        counter <= 50_000_000;
-        led <= !led;
-    end
-    else begin
-        counter <= counter - 1;
-    end
+always @(posedge clk)
+begin
+    if (cnt != 32'd24999999)
+        cnt <= cnt + 32'd1;
+    else
+        cnt <= 32'd0;
 end
+
+wire clign;
+
+always @(posedge clk)
+begin
+    if (clign)
+        led <= ~led;
+end
+
+assign clign = (cnt == 32'd42);
 
 endmodule
