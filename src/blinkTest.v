b2044e0bf8abc38716d9f5c5a53185e6e2377fc2
Sanpi <sanpi@homecomputing.fr>
2013-11-22 20:19:46 +0100

    Remove comments

--- src/blinkTest.v
+++ src/blinkTest.v
@@ -1,32 +1,21 @@
 `timescale 1ns / 1ps
 
 module blinkTest;
-
-    // Inputs
     reg clk;
-
-    // Outputs
     wire led;
 
-    // Instantiate the Unit Under Test (UUT)
     blink uut (
         .clk(clk),
         .led(led)
     );
 
     initial begin
-        // Initialize Inputs
         clk = 0;
 
-        // Wait 100 ns for global reset to finish
-        #100;
-
-        // Add stimulus here
+        #100 $stop;
     end
 
-    // clock signal
     always begin
         #10 clk = !clk;
     end
-
 endmodule
